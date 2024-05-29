6e0446c8b40269f4f0b1d4f28f3da01d4290efe2
Ivan Agustin Arrieta <2217940@ucc.edu.ar>
2024-05-29 01:51:58 -0300

    REVISAR, hice los archivos, en los de inscripcion no pude solucionar el warning y en el de cursos corri el go mod test pero no obtuve los resultados esperados.

--- backend/clients/inscripcion/inscripcion_client.go
+++ backend/clients/inscripcion/inscripcion_client.go
@@ -1,7 +1,44 @@
-package inscripcion
+package inscripciones
 
 import (
+	"backend/model"
+
+	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 var Db *gorm.DB
+
+func CrearInscripcion(inscripcion model.Inscripcion) error {
+	err := Db.Create(&inscripcion).Error
+	if err != nil {
+		log.Error("Error creating inscription:", err)
+		return err
+	}
+	log.Info("Inscription created successfully:", inscripcion)
+	return nil
+}
+
+func GetInscripcionesByUserId(userId int) []model.Inscripcion {
+	var inscripciones []model.Inscripcion
+	Db.Where("id_usuario = ?", userId).Preload("Curso").Find(&inscripciones)
+	log.Debug("Inscripciones: ", inscripciones)
+	return inscripciones
+}
+
+func GetInscripcionesByCursoId(courseId int) []model.Inscripcion {
+	var inscripciones []model.Inscripcion
+	Db.Where("id_curso = ?", courseId).Preload("Usuario").Find(&inscripciones)
+	log.Debug("Inscripciones: ", inscripciones)
+	return inscripciones
+}
+
+func DeleteInscripcionById(inscriptionId int) error {
+	err := Db.Delete(&model.Inscripcion{ID_Inscripcion: inscriptionId}).Error
+	if err != nil {
+		log.Error("Error deleting inscription:", err)
+		return err
+	}
+	log.Info("Inscription deleted successfully:", inscriptionId)
+	return nil
+}
