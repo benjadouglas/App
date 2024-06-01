f339dda172976bf276bdc91e7895e2864dd763e4
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2024-06-01 16:18:18 -0300

    removi archivos frontend

--- frontend/course-ui/app/signUp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EnterForm } from "@/components/enter-form";
-
-export default function signUp() {
-  return (
-    <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
-      <EnterForm isCreatingAccount={true} />
-    </div>
-  );
-}
